c7aae54130ba04c2ca1bedf1bc5538207511a3e1
Artur Bomtempo Colen <arturbcolen@gmail.com>
2026-08-19 11:37:25 -0300

    fix: align extension command with showVerse and display Bible verse message

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,13 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "hello-bible" is now active!');
-
-    const disposable = vscode.commands.registerCommand('hello-bible.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello World from Hello Bible!');
+    const showVerseCommand = vscode.commands.registerCommand('hello-bible.showVerse', () => {
+        vscode.window.showInformationMessage(
+            '📖 João 3:16 - Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna.'
+        );
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(showVerseCommand);
 }
 
 export function deactivate() {}
